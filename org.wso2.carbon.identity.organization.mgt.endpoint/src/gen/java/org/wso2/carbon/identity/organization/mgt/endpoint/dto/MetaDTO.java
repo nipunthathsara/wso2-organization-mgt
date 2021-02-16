49e4a3f42663a827e32d3f0fd061182489afd737
nipunthathsara <nipunthathsara@gmail.com>
2021-02-17 00:02:48 +0530

    Remove metauser from generated code

--- org.wso2.carbon.identity.organization.mgt.endpoint/src/gen/java/org/wso2/carbon/identity/organization/mgt/endpoint/dto/MetaDTO.java
+++ org.wso2.carbon.identity.organization.mgt.endpoint/src/gen/java/org/wso2/carbon/identity/organization/mgt/endpoint/dto/MetaDTO.java
@@ -1,19 +1,17 @@
 /*
  * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
- * WSO2 Inc. licenses this file to you under the Apache License,
- * Version 2.0 (the "License"); you may not use this file except
- * in compliance with the License.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package org.wso2.carbon.identity.organization.mgt.endpoint.dto;
@@ -34,12 +32,6 @@ public class MetaDTO {
 
     @NotNull
     private String lastModified = null;
-
-    @NotNull
-    private MetaUserDTO createdBy = null;
-
-    @NotNull
-    private MetaUserDTO lastModifiedBy = null;
 
     /**
      *
@@ -69,34 +61,6 @@ public class MetaDTO {
         this.lastModified = lastModified;
     }
 
-    /**
-     *
-     **/
-    @ApiModelProperty(required = true,
-                      value = "")
-    @JsonProperty("createdBy")
-    public MetaUserDTO getCreatedBy() {
-        return createdBy;
-    }
-
-    public void setCreatedBy(MetaUserDTO createdBy) {
-        this.createdBy = createdBy;
-    }
-
-    /**
-     *
-     **/
-    @ApiModelProperty(required = true,
-                      value = "")
-    @JsonProperty("lastModifiedBy")
-    public MetaUserDTO getLastModifiedBy() {
-        return lastModifiedBy;
-    }
-
-    public void setLastModifiedBy(MetaUserDTO lastModifiedBy) {
-        this.lastModifiedBy = lastModifiedBy;
-    }
-
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
@@ -104,8 +68,6 @@ public class MetaDTO {
 
         sb.append("  created: ").append(created).append("\n");
         sb.append("  lastModified: ").append(lastModified).append("\n");
-        sb.append("  createdBy: ").append(createdBy).append("\n");
-        sb.append("  lastModifiedBy: ").append(lastModifiedBy).append("\n");
         sb.append("}\n");
         return sb.toString();
     }
